7ba5344051ca3cb8c5665f96c4482bd392259553
Tosin Babatunde <obatos2105@gmail.com>
2023-01-16 21:03:19 -0500

    movedv-4

--- AWS_Code_Pipeline/ec2_instance_bastion.tf
+++ AWS_Code_Pipeline/ec2_instance_bastion.tf
@@ -2,19 +2,17 @@ module "ec2_public" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "4.1.4"
 
-  name          = "Bastion Host"
+  name          = "${var.environment}-BastionHost"
   ami           = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
-  key_name      = "test_key"
+  key_name      = var.instance_keypair
   # monitoring             = true
   vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
   #subnet_id = data.terraform_remote_state.vpc.outputs.public_subnets[0]
 
-  tags = {
-    Name = "Bastion Host"
-  }
+  tags = local.common_tags
 
-  user_data = file("${path.module}/jumpbox-install.sh")
+
 
 }
